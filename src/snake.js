cb2a97ccbd7f6af052b7027d10d822bdc4c68dee
ollie beney <ombeney@gmail.com>
2020-11-25 17:00:40 +0000

    core functionality is there, however, it is currently impossible to lose

--- src/snake.js
+++ src/snake.js
@@ -2,13 +2,15 @@ class Snake {
   constructor(testBody, testTarget, testGridMax) {
     this.maxXCoord = testGridMax || 990
     this.maxYCoord = testGridMax || 740
-    this.body = testBody || [[this.getRandomInt(1,this.maxXCoord), this.getRandomInt(1, this.maxYCoord)]]
-    this.target = testTarget || [this.getRandomInt(1, this.maxXCoord), this.getRandomInt(1, this.maxYCoord)]
+    this.body = testBody || [[Math.round((this.getRandomInt(1,this.maxXCoord)/10))*10, Math.round((this.getRandomInt(1,this.maxYCoord)/10))*10]]
+    this.target = testTarget || [Math.round((this.getRandomInt(1,this.maxXCoord)/10))*10, Math.round((this.getRandomInt(1,this.maxYCoord)/10))*10]
+    this.xChange = 0
+    this.yChange = 0
     //this.renderer = new SnakeRenderer
   }
   
   updateTargetSquare() {
-    this.target = [this.getRandomInt(0, this.maxXCoord), this.getRandomInt(0, this.maxYCoord)]
+    this.target = [Math.round((this.getRandomInt(1,this.maxXCoord)/10))*10, Math.round((this.getRandomInt(1,this.maxYCoord)/10))*10]
     if(this.body.filter(coord => (coord[0] == this.target[0]) && (coord[1] == this.target[1]) ).length > 0) {
       this.updateTargetSquare()
     }
@@ -23,9 +25,25 @@ class Snake {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  moveSnake(coord) {
-    this.body.unshift(coord);
+  gotFood() {
+    // if((this.body[0][0] - this.target[0]) < 15 ) {
+    //   console.log(this.body[0].join('-') + " body :::::::" + this.target +"  target")
+    
+    // }
+    return (this.body[0][0] == this.target[0] && this.body[0][1] == this.target[1])
+  }
+
+  moveSnake() {
+    if(this.gotFood()){
+      snake.body.push(this.target)
+      this.updateTargetSquare()
+    }
+    let newX = this.body[0][0] + this.xChange
+    let newY = this.body[0][1] + this.yChange
+    this.body.unshift([newX,newY]);
+    //this.clearTail()
     this.body.pop();
+    //  console.log(snake.body)
   }
 
   // rendering methods
@@ -37,15 +55,32 @@ class Snake {
     boardCTX.fillRect(this.target[0],this.target[1], 10, 10);
     boardCTX.strokeRect(this.target[0],this.target[1], 10, 10)
   }
+  drawBoard() {
+    const board = document.getElementById('snakeGameCanvas');
+    const boardCTX = board.getContext('2d');
+    boardCTX.fillStyle = "white"
+    boardCTX.fillRect(0,0, 1000, 750)
+  }
+
+//   clearTail() {
+//     const board = document.getElementById('snakeGameCanvas');
+//     const boardCTX = board.getContext('2d');
+//     boardCTX.fillStyle = "red"
+//     let tail = [this.body[this.body.length -1][0], this.body[this.body.length -1][1]]
+    
+//    // boardCTX.clearS(tail[0], tail[1], 10, 10)
+//  //  boardCTX.clearRect(tail[0], tail[1], 15, 15)
+//   }
 
   drawSnake() {
     const board = document.getElementById('snakeGameCanvas')
     const boardCTX = board.getContext('2d')
     boardCTX.fillStyle = 'darkred';
     boardCTX.strokeStyle = 'yellow';
-    for(let i = 0; i <= this.body.length -1; i++) {
+    for(let i = 0; i < this.body.length ; i++) {
       boardCTX.fillRect(this.body[i][0],this.body[i][1], 10, 10);
       boardCTX.strokeRect(this.body[i][0],this.body[i][1], 10, 10)
     }
   }
 }
+let snake = new Snake
